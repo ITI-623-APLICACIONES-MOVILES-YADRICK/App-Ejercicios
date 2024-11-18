7fdc79d9a3f7843dc6d720ae71e416deddada675
Yadrick <ysaguirretr@est.utn.ac.cr>
2024-11-17 22:50:38 -0600

    Acceso a la camara y galeria de fotos

--- app/src/main/java/com/example/appgym/MainActivity.kt
+++ app/src/main/java/com/example/appgym/MainActivity.kt
@@ -1,13 +1,26 @@
 package com.example.appgym
 
 import android.content.Intent
+import android.graphics.Bitmap
 import android.os.Bundle
-import android.text.Editable
-import android.text.TextWatcher
+import android.provider.MediaStore
 import android.widget.Button
 import android.widget.EditText
+import android.widget.ImageButton
+import android.widget.ImageView
 import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.core.content.FileProvider
+import java.io.File
+import java.io.IOException
+import android.content.pm.PackageManager
+import android.net.Uri
+import android.provider.Settings
 
 class MainActivity : AppCompatActivity() {
 
@@ -17,6 +30,17 @@ class MainActivity : AppCompatActivity() {
     private lateinit var generoEditText: EditText
     private lateinit var nacionalidadEditText: EditText
     private lateinit var pesoEditText: EditText
+    private lateinit var imageView: ImageView
+    private lateinit var imageButton: ImageButton
+
+    private var currentPhotoPath: String? = null
+
+    // Lanzadores de actividad para la cámara y galería
+    private lateinit var cameraLauncher: ActivityResultLauncher<Intent>
+    private lateinit var galleryLauncher: ActivityResultLauncher<Intent>
+
+    // Definir el código de solicitud de permisos
+    private val PERMISSION_REQUEST_CODE = 100
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,27 +53,43 @@ class MainActivity : AppCompatActivity() {
         generoEditText = findViewById(R.id.textGenero)
         nacionalidadEditText = findViewById(R.id.textNacionalidad)
         pesoEditText = findViewById(R.id.textPeso)
+        imageView = findViewById(R.id.imageView)
+        imageButton = findViewById(R.id.btn_Photo)
 
-        // Añade TextWatcher para validar en tiempo real
-        edadEditText.addTextChangedListener(object : TextWatcher {
-            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
-            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                if (!isNumeric(s.toString())) {
-                    edadEditText.error = "Formato inválido en Edad"
-                }
-            }
-            override fun afterTextChanged(s: Editable?) {}
-        })
+        // Verifica los permisos de cámara y almacenamiento al inicio
+        checkPermissions()
 
-        pesoEditText.addTextChangedListener(object : TextWatcher {
-            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
-            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                if (!isNumeric(s.toString())) {
-                    pesoEditText.error = "Formato inválido en Peso"
+        // Configura el launcher para la cámara
+        cameraLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == RESULT_OK) {
+                currentPhotoPath?.let {
+                    val imageUri = Uri.parse(it)
+                    imageView.setImageURI(imageUri)
                 }
+            } else {
+                Toast.makeText(this, "No se capturó la imagen", Toast.LENGTH_SHORT).show()
             }
-            override fun afterTextChanged(s: Editable?) {}
-        })
+        }
+
+        // Configura el launcher para la galería
+        galleryLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == RESULT_OK) {
+                val selectedImageUri = result.data?.data
+                try {
+                    selectedImageUri?.let {
+                        imageView.setImageURI(it)
+                    }
+                } catch (e: IOException) {
+                    Toast.makeText(this, "Error al seleccionar la imagen", Toast.LENGTH_SHORT).show()
+                }
+            } else {
+                Toast.makeText(this, "Error al seleccionar la imagen", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        imageButton.setOnClickListener {
+            openCameraOrGallery()
+        }
 
         val VentanaPantalla2: Button = findViewById(R.id.BtnSiguiente)
         VentanaPantalla2.setOnClickListener {
@@ -59,8 +99,57 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    private fun openCameraOrGallery() {
+        val options = arrayOf("Cámara", "Galería")
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Selecciona una opción")
+        builder.setItems(options) { dialog, which ->
+            when (which) {
+                0 -> openCamera()
+                1 -> openGallery()
+            }
+        }
+        builder.show()
+    }
+
+    private fun openCamera() {
+        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        if (intent.resolveActivity(packageManager) != null) {
+            val imageFile: File? = try {
+                createImageFile()
+            } catch (e: IOException) {
+                Toast.makeText(this, "Error al crear el archivo para la imagen", Toast.LENGTH_SHORT).show()
+                null
+            }
+
+            imageFile?.also {
+                val imageUri = FileProvider.getUriForFile(
+                    this,
+                    "com.example.appgym.fileprovider",
+                    it
+                )
+                intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri)
+                currentPhotoPath = imageUri.toString()
+                cameraLauncher.launch(intent)
+            }
+        }
+    }
+
+    private fun openGallery() {
+        val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+        galleryLauncher.launch(intent)
+    }
+
+    @Throws(IOException::class)
+    private fun createImageFile(): File {
+        val imageFileName = "JPEG_${System.currentTimeMillis()}_"
+        val storageDir = getExternalFilesDir(null)
+        return File.createTempFile(imageFileName, ".jpg", storageDir).apply {
+            currentPhotoPath = absolutePath
+        }
+    }
+
     private fun validateFields(): Boolean {
-        // Verifica que los campos no estén vacíos
         if (nombreEditText.text.isEmpty() ||
             apellidosEditText.text.isEmpty() ||
             generoEditText.text.isEmpty() ||
@@ -73,12 +162,46 @@ class MainActivity : AppCompatActivity() {
         return true
     }
 
-    private fun isNumeric(str: String): Boolean {
-        return str.all { it.isDigit() }
-    }
-
     private fun openActivity(activity: Class<*>) {
         val intent = Intent(this, activity)
         startActivity(intent)
+    }
+
+    private fun checkPermissions() {
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED ||
+            ContextCompat.checkSelfPermission(this, android.Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED ||
+            ContextCompat.checkSelfPermission(this, android.Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(android.Manifest.permission.CAMERA, android.Manifest.permission.WRITE_EXTERNAL_STORAGE, android.Manifest.permission.READ_EXTERNAL_STORAGE),
+                PERMISSION_REQUEST_CODE
+            )
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == PERMISSION_REQUEST_CODE) {
+            if (grantResults.isNotEmpty() && grantResults.all { it == PackageManager.PERMISSION_GRANTED }) {
+                Toast.makeText(this, "Permisos otorgados", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this, "Permisos denegados", Toast.LENGTH_SHORT).show()
+                showPermissionDeniedMessage()
+            }
+        }
+    }
+
+    private fun showPermissionDeniedMessage() {
+        AlertDialog.Builder(this)
+            .setMessage("Se necesitan permisos para usar la cámara y almacenamiento. ¿Deseas ir a la configuración?")
+            .setPositiveButton("Sí") { _, _ ->
+                val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+                val uri = Uri.fromParts("package", packageName, null)
+                intent.data = uri
+                startActivity(intent)
+            }
+            .setNegativeButton("No") { dialog, _ -> dialog.dismiss() }
+            .create()
+            .show()
     }
 }
